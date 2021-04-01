4e545523ff742f267056d1042526f889dd5b0cc7
annur25 <hafidz.bdt@gmail.com>
2021-04-01 18:34:50 +0700

    update 1/04/2021

---
+++ p/tema-2/assets/js/indexs.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+    AOS.init();
+    $("#content").hide();
+    $('#bukaUndangan').hide();
+    // $('#waiting').hide();
+    setTimeout(function () {
+        $('#waiting').show();
+    }, 3000);
+    setTimeout(function () {
+        $('#waiting').fadeOut(1000);
+    }, 4000);
+
+    setTimeout(function () {
+        $('#bukaUndangan').fadeIn(1000);
+    }, 6000);
+
+
+    $("#event-page").hide();
+    $("#maps-page").hide();
+    $("#say-page").hide();
+    $("#gallery-page").hide();
+    $("#playButton").hide();
+
+});
+
+$("#bukaTombol").click(function () {
+    $("#protokol").fadeIn(1000);
+    setTimeout(function () {
+        $("#protokol").fadeOut(1000);
+    }, 8000);
+    $("#content").fadeIn(3000);
+    $('#bukaUndangan').hide();
+    var x = document.getElementById("playAudio");
+    var y = document.getElementById("playButton");
+    var z = document.getElementById("stopButton");
+    x.play();
+    y.style.display = "none";
+    z.style.display = "block";
+});
+
+$("#home-button").click(function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    $("#home-page").fadeIn(1000);
+    $("#event-page").hide();
+    $("#maps-page").hide();
+    $("#say-page").hide();
+    $("#gallery-page").hide();
+})
+
+$("#event-button").click(function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    $("#home-page").hide();
+    $("#event-page").fadeIn(1000);
+    $("#maps-page").hide();
+    $("#say-page").hide();
+    $("#gallery-page").hide();
+})
+
+$("#maps-button").click(function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    $("#home-page").hide();
+    $("#event-page").hide();
+    $("#maps-page").fadeIn(1000);
+    $("#say-page").hide();
+    $("#gallery-page").hide();
+})
+
+$("#say-button").click(function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    $("#home-page").hide();
+    $("#event-page").hide();
+    $("#maps-page").hide();
+    $("#say-page").fadeIn(1000);
+    $("#gallery-page").hide();
+})
+
+$("#gallery-button").click(function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    $("#home-page").hide();
+    $("#event-page").hide();
+    $("#maps-page").hide();
+    $("#say-page").hide();
+    $("#gallery-page").fadeIn(1000);
+})
