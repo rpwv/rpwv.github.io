9d04b64d5a4bdda8d41bbf83c3eb2842473ae914
Oby#004 <77336459+rpwv@users.noreply.github.com>
2021-05-09 17:01:31 +0700

    Update countdown.js

--- p/tema-1/assets/js/countdown.js
+++ p/tema-1/assets/js/countdown.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("May 20, 2021 13:00:00").getTime();
+var countDownDate = new Date("May 20, 2021 13:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
